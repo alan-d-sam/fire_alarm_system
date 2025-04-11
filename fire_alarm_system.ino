d462d3ea4ac18cf329bbedb30934eb506dc0f3ef
Alan D Sam <alandsam004@gmail.com>
2025-04-12 01:32:27 +0530

    Create fire_alarm_system.ino
    Arduino code for fire alarm system

---
+++ fire_alarm_system.ino
@@ -0,0 +1,45 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+#define FlamePin A0
+#define buzzer 1
+#define sprinkler 0
+
+void setup() {
+  lcd.begin(16, 2);
+  pinMode(FlamePin, INPUT_PULLUP);
+  pinMode(buzzer, OUTPUT);
+  pinMode (sprinkler, OUTPUT);
+  lcd.setCursor(5, 0);
+  lcd.print("FLAME");
+  lcd.setCursor(3, 1);
+  lcd.print("DETECTOR");
+  delay(1000);
+  lcd.clear();
+}
+
+void loop() {
+
+  int Flame = digitalRead(FlamePin);
+
+  if (Flame == HIGH)
+  {
+    digitalWrite(buzzer, HIGH);
+    digitalWrite(sprinkler, HIGH);
+    lcd.setCursor(5, 0);
+    lcd.print("FLAME");
+    lcd.setCursor(3, 1);
+    lcd.print("DETECTED");
+    delay(200);
+    lcd.clear();
+    delay(200);
+
+  }
+  else
+  {
+    digitalWrite(buzzer, LOW);
+    digitalWrite(sprinkler, LOW);
+
+  }
+}
